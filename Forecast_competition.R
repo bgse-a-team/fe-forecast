c7275313c234c5b8a75b887b011c90eab06ff969
danielbestard <danielbestard@gmail.com>
2017-03-02 15:07:47 +0100

    add some extra models

--- Forecast_competition.R
+++ Forecast_competition.R
@@ -2,13 +2,10 @@ library(readr)
 library(tseries)
 library(lmtest)
 library(sandwich)
-data <- as.data.frame(read_csv("~/Desktop/forecast-competition/forecast-competition-training.csv"))
+data <- as.data.frame(read_csv("~/Desktop/forecast-competition/fe-forecast/forecast-competition-training.csv"))
 
 # Plot TARGET variable
 plot(data[,"TIME"],data[,"TARGET"], col="red",type="l")
-
-lines(data[,"TIME"],ma1$residuals + data[,"TARGET"], col="blue",type="l")
-abline(h=0, lwd=2)
 
 # Check if TARGET is statioary
 adf.test(data[,"TARGET"]) # Evidence of stationarity
@@ -49,6 +46,7 @@ jarque.bera.test(ma1$residuals) # Evidence of normality of the residuals
 # ARMA(1,1) with no predictors
 arma11 <- arima(data[,"TARGET"], order=c(1,0,1))
 arma11$aic
+-2/length(data[,"TARGET"])*(arma11$loglik) + 2/length(data[,"TARGET"])*2
 Box.test(arma11$residuals, lag=22, type="Ljung-Box") # Fail to reject the null hypothesis of independence of the residuals
 acf(arma11$residuals, ylim=c(-0.1,1), lwd=5, xlim=c(0,25), col='darkorange2')
 pacf(arma11$residuals, ylim=c(-0.1,1), lwd=5, xlim=c(0,25), col='darkorange2')
@@ -65,5 +63,78 @@ arma22$aic
 # ARMA(1,2) with no predictors
 arma12 <- arima(data[,"TARGET"], order=c(1,0,2))
 arma12$aic
+
+# Study of volatility clustering
+# Plot absolute values of the TARGET variable
+plot(data[,"TIME"],abs(data[,"TARGET"]), col="red",type="l")
+
+# Sample autocorrelation of obsolute returns
+acf(abs(data[,"TARGET"]), ylim=c(-0.1,1), lwd=5, xlim=c(0,25), col='darkorange2') # There seems to be some dependence!
+
+# Sample autocorrelation of obsolute returns
+acf(data[,"TARGET"]^2, ylim=c(-0.1,1), lwd=5, xlim=c(0,25), col='darkorange2') # There seems to be some dependence!
+
+# ARCH-LM test (check whether there is statistical significance of volatility clustering)
+# For 5 lagged square returns
+y <- data[,"TARGET"][6:length(data[,"TARGET"])]**2
+n <- length(y)
+X <- cbind(data[,"TARGET"][5:(length(data[,"TARGET"])-1)]**2, data[,"TARGET"][4:(length(data[,"TARGET"])-2)]**2,
+          data[,"TARGET"][3:(length(data[,"TARGET"])-3)]**2, data[,"TARGET"][2:(length(data[,"TARGET"])-4)]**2,
+          data[,"TARGET"][1:(length(data[,"TARGET"])-5)]**2)
+archlm <- lm(y ~ X)
+archlm.statistic <- n*summary(archlm)$r.squared
+1-pchisq(archlm.statistic,5) # Reject null hypothesis of absence of volatility clustering
+
+# For 4 lagged square returns
+y <- data[,"TARGET"][5:length(data[,"TARGET"])]**2
+n <- length(y)
+X <- cbind(data[,"TARGET"][4:(length(data[,"TARGET"])-1)]**2,
+           data[,"TARGET"][3:(length(data[,"TARGET"])-2)]**2,
+           data[,"TARGET"][2:(length(data[,"TARGET"])-3)]**2,
+           data[,"TARGET"][1:(length(data[,"TARGET"])-4)]**2)
+archlm <- lm(y ~ X)
+archlm.statistic <- n*summary(archlm)$r.squared
+1-pchisq(archlm.statistic,4) # Reject null hypothesis of absence of volatility clustering
+
+# For 3 lagged square returns
+y <- data[,"TARGET"][4:length(data[,"TARGET"])]**2
+n <- length(y)
+X <- cbind(data[,"TARGET"][3:(length(data[,"TARGET"])-1)]**2,
+           data[,"TARGET"][2:(length(data[,"TARGET"])-2)]**2,
+           data[,"TARGET"][1:(length(data[,"TARGET"])-3)]**2)
+archlm <- lm(y ~ X)
+archlm.statistic <- n*summary(archlm)$r.squared
+1-pchisq(archlm.statistic,3) # Reject null hypothesis of absence of volatility clustering
+
+# For 2 lagged square returns
+y <- data[,"TARGET"][3:length(data[,"TARGET"])]**2
+n <- length(y)
+X <- cbind(data[,"TARGET"][2:(length(data[,"TARGET"])-1)]**2,
+           data[,"TARGET"][1:(length(data[,"TARGET"])-2)]**2)
+archlm <- lm(y ~ X)
+archlm.statistic <- n*summary(archlm)$r.squared
+1-pchisq(archlm.statistic,2) # Reject null hypothesis of absence of volatility clustering
+
+# For 1 lagged square returns
+y <- data[,"TARGET"][2:length(data[,"TARGET"])]**2
+n <- length(y)
+X <- cbind(data[,"TARGET"][1:(length(data[,"TARGET"])-1)]**2)
+archlm <- lm(y ~ X)
+archlm.statistic <- n*summary(archlm)$r.squared
+1-pchisq(archlm.statistic,1) # Reject null hypothesis of absence of volatility clustering
+
+# GARCH(1,1) with no predictors
+garch11 <- garch(data[,"TARGET"], order = c(1,1))
+aic <- -2/length(data[,"TARGET"])*(-garch11$n.likeli) + 2/length(data[,"TARGET"])*3
+aic
+
+
+
+
+
+
+
+
+
 
 
