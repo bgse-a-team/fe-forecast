8a35e26a3621fe084e7432038d6b98c4958d7e9b
Michael Cameron <michael.cameron@barcelonagse.eu>
2017-03-06 15:16:15 +0100

    final function

--- function.R
+++ function.R
@@ -1,7 +1,13 @@
 predictor.one <- function(data){
-  library(glmnet)
-  T <- nrow(data)
-  fit <- glmnet(as.matrix(cbind(data[2:T,2:50], data[1:(T-1),1])), as.matrix(data[2:T,1]), alpha = 1, family = "mgaussian")
-  pred<-as.numeric(predict.glmnet(fit, newx =as.matrix(data[T,1:50]), s=0, type="link"))
+  x <- prcomp(data[1:nrow(data),2:ncol(data)], center=T, scale.=T)
+  principal_components <- x$x[,1:4]
+  x1 <- data[1:(nrow(data) - 1),"TARGET"]
+  data_new <- as.data.frame(cbind(data[2:nrow(data),"TARGET"],x1,principal_components[1:(nrow(data)-1),, drop=F]))
+  colnames(data_new) <- c("y","x1",colnames(principal_components[1:(nrow(data)-1),, drop=F]))
+  model <- lm(y ~ ., data=data_new)
+  newdata <- cbind(data[nrow(data),"TARGET"],principal_components[nrow(principal_components),, drop=F])
+  colnames(newdata)[1] <- "x1"
+  newdata <- as.data.frame(newdata)
+  pred<-predict(model, newdata=newdata)
   return( pred )
 }
